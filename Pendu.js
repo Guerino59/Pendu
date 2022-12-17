44e5f046de802ff9e7201cabff126c9fda516e50
Your Name <you@example.com>
2022-12-17 21:35:08 +0100

    wow

--- Pendu.js
+++ Pendu.js
@@ -7,60 +7,95 @@ export default class Pendu {
     this.buttons = document.querySelectorAll("button");
     this.buttons.forEach((b) => {
       b.addEventListener("click", () => {
-        this.jeu();
+        this.notLetterInWord(b);
         this.letterInWord(b);
+        this.result();
+        console.log(this.mot.textContent);
       });
     });
     this.nbLetter = document.querySelector(".nbLetter");
     this.essais = document.querySelector(".nbError");
     this.mot = document.querySelector(".mot");
-    this.findingWord = "avachi";
+    this.words = [
+      "pizza",
+      "apple",
+      "banana",
+      "strawberry",
+      "orange",
+      "pineapple",
+      "grapes",
+    ];
+    this.findingWord = "";
+    this.randomWord();
     this.arrLetter = [];
-    this.falseLetter = [];
     this.nbErr = 0;
     this.nbLetter.textContent = `Le mot contient ${this.findingWord.length} lettres`;
     this.push();
     this.create();
-    console.log(this.arrLetter);
-    this.jeu();
   }
+
   push() {
-    for (this.i = 0; this.i < this.findingWord.length; this.i++) {
-      this.arrLetter.push(this.findingWord[this.i]);
+    for (let i = 0; i < this.findingWord.length; i++) {
+      this.arrLetter.push(this.findingWord[i]);
     }
   }
+
   create() {
-    for (this.i = 0; this.i < this.arrLetter.length; this.i++) {
+    for (let i = 0; i < this.arrLetter.length; i++) {
       this.letter = document.createElement("span");
       this.letter.textContent = "-";
       this.mot.append(this.letter);
     }
   }
-  revealFinal() {
-    for (this.i = 0; this.i < this.arrLetter.length; this.i++) {
-      this.smot = document.querySelectorAll(".mot span");
-      this.smot[this.i].textContent = this.arrLetter[this.i];
-      console.log(this.smot);
+  randomWord() {
+    {
+      this.findingWord =
+        this.words[Math.floor(Math.random() * this.words.length)];
     }
   }
-  jeu() {
+  isStringOnlyLetters(str) {
+    return /^[a-zA-Z]+$/.test(str);
+  }
+
+  revealFinal() {
+    for (let i = 0; i < this.arrLetter.length; i++) {
+      this.smot = document.querySelectorAll(".mot span");
+      this.smot[i].textContent = this.arrLetter[i];
+    }
+  }
+
+  result() {
     this.essais.textContent = `${this.nbErr} erreurs`;
-    this.nbErr++;
-    if (this.nbErr > 9) {
-      prompt("PERDU");
+    if (this.isStringOnlyLetters(this.mot.textContent)) {
+      this.essais.textContent = "YOU WIN";
+      this.essais.style.color = "green";
+      this.mot.style.animationName = "bordr";
+    }
+    if (this.nbErr > 7) {
+      this.essais.textContent = "YOU LOSE";
+      this.essais.style.color = "red";
+      this.mot.style.border = "10px solid red";
       this.revealFinal();
       this.nbErr = 0;
     }
   }
-  reveaLetter(e) {
-    this.smot = e.textContent;
+  notLetterInWord(e) {
+    this.selectedLetters = e.textContent;
+    if (!this.arrLetter.includes(this.selectedLetters)) {
+      e.style.pointerEvents = "none";
+      e.style.backgroundColor = "rgba(204,29,29,0.5)";
+      this.nbErr++;
+      this.essais.textContent = `${this.nbErr} erreurs`;
+    }
   }
   letterInWord(e) {
     this.selectedLetters = e.textContent;
-    for (this.i = 0; this.i < this.arrLetter.length; this.i++) {
+    for (let i = 0; i < this.arrLetter.length; i++) {
       this.smot = document.querySelectorAll(".mot span");
-      if (this.selectedLetters === this.arrLetter[this.i]) {
-        this.smot[this.i].textContent = this.selectedLetters;
+      if (this.selectedLetters === this.arrLetter[i]) {
+        this.smot[i].textContent = this.selectedLetters;
+        e.style.backgroundColor = "rgba(29,204,66,0.5)";
+        e.style.pointerEvents = "none";
       }
     }
   }
