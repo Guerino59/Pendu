b65c32c1ab0f9d1520e09242b48abb87f6b9f09e
unknown <aaa@gmail.com>
2022-12-19 12:31:52 +0100

    m

--- Pendu.js
+++ Pendu.js
@@ -4,43 +4,50 @@
 
 export default class Pendu {
   constructor() {
+    // selection de tous les boutons
     this.buttons = document.querySelectorAll("button");
+    // boucle pour recuperer le contenu de chaque boutons
     this.buttons.forEach((b) => {
       b.addEventListener("click", () => {
-        this.notLetterInWord(b);
         this.letterInWord(b);
         this.result();
-        console.log(this.mot.textContent);
       });
     });
+    // fonction reset
+    this.restart = document.querySelector('.restart');
+    this.restart.addEventListener('click', this.reset.bind(this));
+    // selction de mes elements html
     this.nbLetter = document.querySelector(".nbLetter");
     this.essais = document.querySelector(".nbError");
     this.mot = document.querySelector(".mot");
     this.img = document.querySelector(".anim");
-    this.words = [
-      "pizza",
-      "apple",
-      "banana",
-      "strawberry",
-      "orange",
-      "pineapple",
-      "grapes",
-    ];
+    this.smot = document.querySelectorAll(".mot span");
+    //Fetch
+    this.url = "./mots.json"
+    fetch(this.url).then(this.haFetch.bind(this));
+    // variable mot à trouver
     this.findingWord = "";
-    this.randomWord();
-    this.arrLetter = [];
-    this.nbErr = 0;
-    this.nbLetter.textContent = `Mot : ${this.findingWord.length} lettres`;
-    this.push();
-    this.create();
   }
-
+  //Fetch prenant un mot random dans le fichier ./mot.json
+  haFetch(responseText)
+  {
+    if(responseText.ok)
+    {
+       responseText.json()
+            .then(this.randomWord.bind(this))
+            .catch(error=>console.log(error))
+    }
+    else{
+        console.log(responseText.statusText);
+    }
+ }
+ //fonction permettant d'ajouter les lettres de mon mot dans un tableau "arrLetter"
   push() {
     for (let i = 0; i < this.findingWord.length; i++) {
       this.arrLetter.push(this.findingWord[i]);
     }
   }
-
+// fonction permettant de creer autant de span que de lettre presente dans mon tableau et les remplaçés par des "-"
   create() {
     for (let i = 0; i < this.arrLetter.length; i++) {
       this.letter = document.createElement("span");
@@ -48,39 +55,63 @@ export default class Pendu {
       this.mot.append(this.letter);
     }
   }
-  randomWord() {
+  // fonction permettant de choisir un mot aleatoirement et declaration de variable + appel des deux fonctions precedentes
+  randomWord(data) {
     {
+      console.log(data);
+      
       this.findingWord =
-        this.words[Math.floor(Math.random() * this.words.length)];
+        data[Math.floor(Math.random() * data.length)];
+        this.arrLetter = [];
+        this.nbErr = 0;
+        this.nbLetter.textContent = `Mot : ${this.findingWord.length} lettres`;
+        this.push();
+        this.create();
+        
     }
   }
+  // verifie a chaque fois qu'un boutton est appuyé si le contenu de mon mot est composé uniquement de lettre
   isStringOnlyLetters(str) {
     return /^[a-zA-Z]+$/.test(str);
   }
-
+// fonction permettant de reveler l'entiereté du mot si on perd
   revealFinal() {
     for (let i = 0; i < this.arrLetter.length; i++) {
-      this.smot = document.querySelectorAll(".mot span");
       this.smot[i].textContent = this.arrLetter[i];
     }
   }
-
+// fonction permettant de verifier si on a gagné ou perdu
   result() {
     this.essais.textContent = `${this.nbErr} erreurs`;
     if (this.isStringOnlyLetters(this.mot.textContent)) {
       this.essais.textContent = "YOU WIN";
       this.essais.style.color = "green";
       this.mot.style.animationName = "bordr";
+      this.restart.style.top = "53%"
+      for(let i=0; i < this.buttons.length; i++)
+    {
+    this.buttons[i].style.backgroundColor = "";
+    this.buttons[i].style.pointerEvents = "";
+    }
+      this.nbErr = 0;
+      console.log(this.nbErr);
     }
     if (this.nbErr > 7) {
       this.essais.textContent = "YOU LOSE";
       this.essais.style.color = "red";
       this.mot.style.border = "10px solid red";
       this.revealFinal();
+      this.restart.style.top = "53%"
+      for(let i=0; i < this.buttons.length; i++)
+    {
+    this.buttons[i].style.backgroundColor = "";
+    this.buttons[i].style.pointerEvents = "";
+    }
       this.nbErr = 0;
     }
   }
-  notLetterInWord(e) {
+  //fonction se declenchant au clic sur un de mes boutons permettant de verifier si la lettre appuyé est presente ou non dans mon tableau
+  letterInWord(e) {
     this.selectedLetters = e.textContent;
     if (!this.arrLetter.includes(this.selectedLetters)) {
       e.style.pointerEvents = "none";
@@ -90,9 +121,6 @@ export default class Pendu {
       this.img.style.backgroundSize = "cover";
       this.essais.textContent = `${this.nbErr} erreurs`;
     }
-  }
-  letterInWord(e) {
-    this.selectedLetters = e.textContent;
     for (let i = 0; i < this.arrLetter.length; i++) {
       this.smot = document.querySelectorAll(".mot span");
       if (this.selectedLetters === this.arrLetter[i]) {
@@ -101,5 +129,24 @@ export default class Pendu {
         e.style.pointerEvents = "none";
       }
     }
+  }
+  // Fonction permettant de reset le jeu
+  reset()
+  {
+    this.findingWord = "";
+    this.arrLetter = [];
+    this.nbErr = 0;
+    this.essais.textContent = "";
+    this.essais.style.color = "";
+    this.mot.style.animationName = "";
+    this.mot.style.border = "";
+    this.img.style.background = `url(./img-pendu/Le-Pendu0.png)`;
+    this.img.style.backgroundSize = "";
+    this.mot.textContent = "";
+    this.restart.style.top = "-50%"
+    fetch(this.url).then(this.haFetch.bind(this));
+    this.push();
+    this.create();
+    this.nbLetter.textContent = `Mot : ${this.findingWord.length} lettres`;
   }
 }
