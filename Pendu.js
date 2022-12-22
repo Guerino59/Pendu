47beba9ca246fa8743e25e559fe01c32a48ca674
unknown <aaa@gmail.com>
2022-12-22 16:09:23 +0100

    o

--- Pendu.js
+++ Pendu.js
@@ -13,6 +13,9 @@ export default class Pendu {
         this.result();
       });
     });
+    //canvas
+    this.canvas = document.querySelector('canvas');
+    this.ctx = this.canvas.getContext('2d');
     // fonction reset
     this.restart = document.querySelector('.restart');
     this.restart.addEventListener('click', this.reset.bind(this));
@@ -22,6 +25,9 @@ export default class Pendu {
     this.mot = document.querySelector(".mot");
     this.img = document.querySelector(".anim");
     this.smot = document.querySelectorAll(".mot span");
+    
+    this.resize();
+    this.draw();
     //Fetch
     this.url = "./mots.json"
     fetch(this.url).then(this.haFetch.bind(this));
@@ -41,6 +47,78 @@ export default class Pendu {
         console.log(responseText.statusText);
     }
  }
+ //resize de mon canvas
+ resize()
+ {
+   this.canvas.width = this.img.getBoundingClientRect().width;
+   this.canvas.height = this.img.getBoundingClientRect().height; 
+ }
+ draw(erreur)
+ {
+  this.ctx.beginPath()
+  this.ctx.strokeStyle = "black";
+  this.ctx.lineWidth = 8;
+  this.ctx.lineCap = "round";
+  this.ctx.moveTo(45, 280)
+  this.ctx.lineTo(255, 280)
+  this.ctx.moveTo(235, 280)
+  this.ctx.lineTo(235, 50)
+  this.ctx.lineTo(105, 50)
+  this.ctx.stroke()
+  if(erreur == 1){  
+  this.ctx.lineTo(105, 80)
+  this.ctx.stroke()
+  }  
+  if(erreur == 2){
+  this.ctx.beginPath() 
+  this.ctx.arc(105, 100, 28, 0, 2*Math.PI)
+  this.ctx.fill()
+  }
+  if(erreur == 3){
+  this.ctx.beginPath() 
+  this.ctx.moveTo(105, 100)
+  this.ctx.lineTo(105, 210)
+  this.ctx.stroke()
+  }
+  if(erreur == 4){ 
+  this.ctx.moveTo(105,150)
+  this.ctx.lineTo(145, 180)
+  this.ctx.stroke()
+  }
+  if(erreur == 5){ 
+  this.ctx.moveTo(105,150)
+  this.ctx.lineTo(65, 180)
+  this.ctx.stroke()
+  }
+  if(erreur == 6){ 
+  this.ctx.moveTo(105, 210)
+  this.ctx.lineTo(135, 250)
+  this.ctx.stroke()
+  }
+  if(erreur == 7){ 
+  this.ctx.moveTo(105, 210)
+  this.ctx.lineTo(75, 250)
+  this.ctx.stroke()
+  }
+  if(erreur > 7)
+  {
+    this.ctx.beginPath()
+    this.ctx.lineWidth = 4
+    this.ctx.strokeStyle = "red"
+    this.ctx.moveTo(100, 90)
+    this.ctx.lineTo(90, 100)
+    this.ctx.moveTo(100, 100)
+    this.ctx.lineTo(90, 90)
+    this.ctx.moveTo(110, 90)
+    this.ctx.lineTo(120, 100)
+    this.ctx.moveTo(120, 90)
+    this.ctx.lineTo(110, 100)
+    this.ctx.stroke()
+    
+  }
+  //
+
+ }
  //fonction permettant d'ajouter les lettres de mon mot dans un tableau "arrLetter"
   push() {
     for (let i = 0; i < this.findingWord.length; i++) {
@@ -57,11 +135,8 @@ export default class Pendu {
   }
   // fonction permettant de choisir un mot aleatoirement et declaration de variable + appel des deux fonctions precedentes
   randomWord(data) {
-    {
-      console.log(data);
-      
-      this.findingWord =
-        data[Math.floor(Math.random() * data.length)];
+    { 
+        this.findingWord = data[Math.floor(Math.random() * data.length)];
         this.arrLetter = [];
         this.nbErr = 0;
         this.nbLetter.textContent = `Mot : ${this.findingWord.length} lettres`;
@@ -84,10 +159,11 @@ export default class Pendu {
   result() {
     this.essais.textContent = `${this.nbErr} erreurs`;
     if (this.isStringOnlyLetters(this.mot.textContent)) {
-      this.essais.textContent = "YOU WIN";
+      this.essais.textContent = "C'est gagné !";
       this.essais.style.color = "green";
-      this.mot.style.animationName = "bordr";
-      this.restart.style.top = "53%"
+      this.restart.style.top = "49%"
+      this.restart.style.borderColor = "green"
+      this.smot.forEach(s=>{s.style.color = "green"})
       for(let i=0; i < this.buttons.length; i++)
     {
     this.buttons[i].style.backgroundColor = "";
@@ -97,11 +173,12 @@ export default class Pendu {
       console.log(this.nbErr);
     }
     if (this.nbErr > 7) {
-      this.essais.textContent = "YOU LOSE";
+      this.essais.textContent = "C'est perdu !";
       this.essais.style.color = "red";
-      this.mot.style.border = "10px solid red";
+      this.smot.forEach(s=>{s.style.color = "red"})
       this.revealFinal();
-      this.restart.style.top = "53%"
+      this.restart.style.top = "49%"
+      this.restart.style.borderColor = "red"
       for(let i=0; i < this.buttons.length; i++)
     {
     this.buttons[i].style.backgroundColor = "";
@@ -117,7 +194,9 @@ export default class Pendu {
       e.style.pointerEvents = "none";
       e.style.backgroundColor = "rgba(204,29,29,0.5)";
       this.nbErr++;
-      this.img.style.background = `url(./img-pendu/Le-Pendu${this.nbErr}.png)`;
+      this.draw(this.nbErr);
+      console.log(this.nbErr);
+      // this.img.style.background = `url(./img-pendu/Le-Pendu${this.nbErr}.png)`;
       this.img.style.backgroundSize = "cover";
       this.essais.textContent = `${this.nbErr} erreurs`;
     }
@@ -140,7 +219,8 @@ export default class Pendu {
     this.essais.style.color = "";
     this.mot.style.animationName = "";
     this.mot.style.border = "";
-    this.img.style.background = `url(./img-pendu/Le-Pendu0.png)`;
+    this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+    this.draw();
     this.img.style.backgroundSize = "";
     this.mot.textContent = "";
     this.restart.style.top = "-50%"
